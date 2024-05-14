b54ac49d7285996d97359fc468868255591bc142
fahim <fahimrahman408@gmail.com>
2024-05-14 09:58:29 +0600

    searchbar in seller dashboard

--- ShowSearch.cs
+++ ShowSearch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,17 +11,45 @@ using System.Windows.Forms;
 
 namespace EliteEstates
 {
-   
     public partial class ShowSearch : Form
     {
         private string productName;
-        private string connectionString = "Data Source=USER\\SQLEXPRESS;Initial Catalog=EliteEstates;Integrated Security=True"; // Replace with your actual connection string
+        private string connectionString = "Data Source=USER\\SQLEXPRESS;Initial Catalog=EliteEstates;Integrated Security=True";
 
         public ShowSearch(string productName)
         {
             InitializeComponent();
             this.productName = productName;
-           // LoadSearchResults();
+            LoadSearchResults();
+        }
+
+        private void LoadSearchResults()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"SELECT [Productname], [Price], [Totalroom], [Size], [Location], [ProductID], [SellerID], [Sellername]
+                                     FROM [dbo].[Product]
+                                     WHERE [Productname] LIKE @ProductName";
+
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@ProductName", "%" + productName + "%");
+
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    searchitemshowgrid.Columns.Clear();
+
+                    searchitemshowgrid.DataSource = dataTable;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading search results: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
